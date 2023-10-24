a653e379372241071ec11c31222a94a432a131eb
piyush4501 <140049226+piyush4501@users.noreply.github.com>
2023-10-25 01:51:35 +0530

    Conainer done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountID = 14478
+let accountSTRENGTH = "medium"
+var accountname = "piyush"
+accountDESIRE = "money"
+
+
+// accountID = 15669
+accountname = "harsh raj"
+accountDESIRE = "Love"
+accountSTRENGTH = "stronge"
+
+/* we use let over var besause var can not understand scope */
+
+console.table([accountID, accountSTRENGTH, accountname, accountDESIRE]);
